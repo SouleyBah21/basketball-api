7187e42a450456fe458bddd28921553c48e23411
Souleymane Bah <souley2183@gmail.com>
2026-05-19 16:21:06 -0400

    Add get player by id route

--- routers/players.py
+++ routers/players.py
@@ -10,4 +10,12 @@ def create_player(player:PlayerCreate):
     conn.commit()
     return{"message":"Player created"}
 
+@router.get("/players/{player_id}")
+def get_player(player_id:int):
+    cursor.execute("SELECT * FROM PLAYERS WHERE ID = %s",(player_id,))
+    player = cursor.fetchone()
+    if not player:
+        raise HTTPException(status_code = 404, detail = "Player not found")
+    return player
+    
 
